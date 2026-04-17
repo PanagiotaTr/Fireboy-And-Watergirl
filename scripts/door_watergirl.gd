2c2c578bd3f2ade6190c7b6b6004ca6b3ef5c9a6
george_sorogkas <georgesor1@hotmail.com>
2026-04-17 06:20:31 +0300

    Door scenes and scripts

---
+++ scripts/door_watergirl.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@export var allowed_group: String = "watergirl"
+
+@onready var trigger: Area2D = $Trigger
+@onready var anim: AnimationPlayer = $AnimationPlayer
+@onready var front_parts: Node2D = $FrontParts
+
+func _ready() -> void:
+	trigger.body_entered.connect(_on_body_entered)
+	trigger.body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group(allowed_group):
+		anim.play("open")
+
+func _on_body_exited(body: Node) -> void:
+	if body.is_in_group(allowed_group):
+		for n in range(7):
+			front_parts.get_child(n).visible = false
+		anim.play_backwards("close2")
