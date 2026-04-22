c7d4d3831c52e2e55175dcbae07d083b4eb9d913
Panagiota.tr <panayiota03@gmail.com>
2026-04-22 23:20:47 +0300

    fix timer

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -5,7 +5,31 @@ var minutes: int = 0
 var seconds: int = 0
 @onready var minutes_text: Label = $MinutesText
 @onready var seconds_text: Label = $SecondsText
+@onready var colon_text: Label = $Label
 
+func _ready() -> void:
+	var gold = Color("#f2c94c")
+	var black = Color("#000000")
+
+	minutes_text.label_settings = LabelSettings.new()
+	seconds_text.label_settings = LabelSettings.new()
+	colon_text.label_settings = LabelSettings.new()
+
+	minutes_text.label_settings.font_color = gold
+	minutes_text.label_settings.outline_color = black
+	minutes_text.label_settings.outline_size = 6
+
+	seconds_text.label_settings.font_color = gold
+	seconds_text.label_settings.outline_color = black
+	seconds_text.label_settings.outline_size = 6
+
+	colon_text.label_settings.font_color = gold
+	colon_text.label_settings.outline_color = black
+	colon_text.label_settings.outline_size = 6
+	
+	minutes_text.label_settings.font_size = 35
+	seconds_text.label_settings.font_size = 35
+	colon_text.label_settings.font_size = 35
 
 func _process(delta: float) -> void:
 	time += delta
