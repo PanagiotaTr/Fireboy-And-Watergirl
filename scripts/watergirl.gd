461fcefb1cd330f94e450ea81b6107d8bf085e51
george_sorogkas <georgesor1@hotmail.com>
2026-05-16 00:37:03 +0300

    Increased players speed and jump

--- scripts/watergirl.gd
+++ scripts/watergirl.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-const SPEED = 150.0
-const JUMP_VELOCITY = -350.0
+const SPEED = 160.0
+const JUMP_VELOCITY = -360.0
 
 @onready var body_sprite = $Body
 @onready var head_sprite = $Head
