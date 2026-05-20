93cffba5f980cd578de27d68199facdf7c8d6f0f
Panagiota.tr <panayiota03@gmail.com>
2026-05-20 20:51:12 +0300

    fix dark mode

--- scripts/level_6.gd
+++ scripts/level_6.gd
@@ -3,9 +3,14 @@ extends Node2D
 @onready var fireboy = $Fireboy
 @onready var watergirl = $Watergirl
 @onready var dark_rect = $DarknessLayer/ColorRect
-@onready var camera = $Camera2D
 
-func _process(delta):
+
+func world_to_screen(pos: Vector2) -> Vector2:
+	return get_viewport().get_canvas_transform() * pos
+
+
+func _process(_delta):
+
 	if !is_instance_valid(fireboy):
 		fireboy = get_node_or_null("Fireboy")
 
@@ -13,19 +18,20 @@ func _process(delta):
 		watergirl = get_node_or_null("Watergirl")
 
 	var mat = dark_rect.material
-	var screen_center = get_viewport_rect().size / 2
-	
-	var extra_light_pos = Vector2(575, 15)
-	mat.set_shader_parameter("light_pos_3", extra_light_pos)
 
+	# Timer light
+	mat.set_shader_parameter("light_pos_3", Vector2(955, 20))
+
+	# Fireboy light
 	if is_instance_valid(fireboy) and !fireboy.is_dead:
-		var fireboy_screen_pos = fireboy.global_position - camera.global_position + screen_center
+		var fireboy_screen_pos = world_to_screen(fireboy.global_position) + Vector2(0, -15)
 		mat.set_shader_parameter("light_pos_1", fireboy_screen_pos)
 	else:
 		mat.set_shader_parameter("light_pos_1", Vector2(-10000, -10000))
 
+	# Watergirl light
 	if is_instance_valid(watergirl) and !watergirl.is_dead:
-		var watergirl_screen_pos = watergirl.global_position - camera.global_position + screen_center
+		var watergirl_screen_pos = world_to_screen(watergirl.global_position) + Vector2(0, -15)
 		mat.set_shader_parameter("light_pos_2", watergirl_screen_pos)
 	else:
 		mat.set_shader_parameter("light_pos_2", Vector2(-10000, -10000))
--- scripts/level_9.gd
+++ scripts/level_9.gd
@@ -1,30 +1,25 @@
 extends Node2D
+
 @onready var watergirl: CharacterBody2D = $Watergirl
 @onready var fireboy: CharacterBody2D = $Fireboy
 @onready var dark_rect = $DarknessLayer/ColorRect
 @onready var camera : Camera2D = $Camera2D
 
-func _process(delta):
-	if !is_instance_valid(fireboy):
-		fireboy = get_node_or_null("Fireboy")
 
-	if !is_instance_valid(watergirl):
-		watergirl = get_node_or_null("Watergirl")
+func world_to_screen(pos: Vector2) -> Vector2:
+	return get_viewport().get_canvas_transform() * pos
+
+
+func _process(delta):
 
 	var mat = dark_rect.material
-	var screen_center = get_viewport_rect().size / 2
-	
-	var extra_light_pos = Vector2(575, 15)
-	mat.set_shader_parameter("light_pos_3", extra_light_pos)
-
-	if is_instance_valid(fireboy) and !fireboy.is_dead:
-		var fireboy_screen_pos = fireboy.global_position - camera.global_position + screen_center
-		mat.set_shader_parameter("light_pos_1", fireboy_screen_pos)
-	else:
-		mat.set_shader_parameter("light_pos_1", Vector2(-10000, -10000))
 
 	if is_instance_valid(watergirl) and !watergirl.is_dead:
-		var watergirl_screen_pos = watergirl.global_position - camera.global_position + screen_center
-		mat.set_shader_parameter("light_pos_2", watergirl_screen_pos)
-	else:
-		mat.set_shader_parameter("light_pos_2", Vector2(-10000, -10000))
+		var watergirl_screen_pos = world_to_screen(watergirl.global_position) + Vector2(0, -15)
+		mat.set_shader_parameter("light_pos_1", watergirl_screen_pos)
+
+	if is_instance_valid(fireboy) and !fireboy.is_dead:
+		var fireboy_screen_pos = world_to_screen(fireboy.global_position) + Vector2(0, -15)
+		mat.set_shader_parameter("light_pos_2", fireboy_screen_pos)
+
+	mat.set_shader_parameter("light_pos_3", Vector2(955, 20))
